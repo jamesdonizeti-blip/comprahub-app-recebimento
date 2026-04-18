9cb3ce6e4855c38f40bd4e5153bc48d15ac65676
James Silva <happeningti@gmail.com>
2026-04-18 15:32:19 -0300

    atualização config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@
 export const APP_CONFIG = {
   apiBaseUrl: process.env.EXPO_PUBLIC_API_URL ?? "https://portal.solucaocompras.com.br",
   supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? "https://fdkuynzlbhvdybckhouf.supabase.co",
-  supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZka3V5bnpsYmh2ZHliY2tob3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzA5MjYsImV4cCI6MjA4OTYwNjkyNn0.PKf9JI43NnRnqAFxbEptSR9gPy67EIzs5OD31fic86U",
+  supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "",
 };
